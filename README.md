3eac9418c54c8ea2f1b573b069ffbeda77b66e5c
BigETI <bigeti.ethem@gmail.com>
2019-09-03 22:16:08 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Unity Texture Serializer
+
+## Description
+This package is a texture serializer for Unity.
+The goal is to serialize textures, that can be used within save files or for networking.
+
+## License
+[The license](https://github.com/BigETI/UnityTextureSerializer/blob/master/LICENSE) for this package is available at https://github.com/BigETI/UnityTextureSerializer/blob/master/LICENSE
---
+++ Scripts/Data/SerializedTextureData.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Unity texture serializer data namespace
+/// </summary>
+namespace UnityTextureSerializer.Data
+{
+    /// <summary>
+    /// Serialized texture data class
+    /// </summary>
+    [Serializable]
+    public class SerializedTextureData
+    {
+        /// <summary>
+        /// Size
+        /// </summary>
+        [SerializeField]
+        private Vector2Int size = Vector2Int.one;
+
+        /// <summary>
+        /// Texture format
+        /// </summary>
+        [SerializeField]
+        private TextureFormat textureFormat;
+
+        /// <summary>
+        /// Mip count
+        /// </summary>
+        [SerializeField]
+        private int mipCount = default;
+
+        /// <summary>
+        /// Is linear
+        /// </summary>
+        [SerializeField]
+        private bool linear = default;
+
+        /// <summary>
+        /// PNG data
+        /// </summary>
+        [SerializeField]
+        private string pngData;
+
+        /// <summary>
+        /// Texture
+        /// </summary>
+        private Texture2D texture;
+
+        /// <summary>
+        /// Sprite
+        /// </summary>
+        private Sprite sprite;
+
+        /// <summary>
+        /// Size
+        /// </summary>
+        public Vector2Int Size => size;
+
+        /// <summary>
+        /// PNG data
+        /// </summary>
+        public string PNGData
+        {
+            get
+            {
+                if (pngData == null)
+                {
+                    texture = new Texture2D(size.x, size.y);
+                    pngData = Convert.ToBase64String(texture.EncodeToPNG());
+                }
+                else if (pngData.Length <= 0)
+                {
+                    texture = new Texture2D(size.x, size.y);
+                    pngData = Convert.ToBase64String(texture.EncodeToPNG());
+                }
+                return pngData;
+            }
+        }
+
+        /// <summary>
+        /// Texture
+        /// </summary>
+        public Texture2D Texture
+        {
+            get
+            {
+                if (texture == null)
+                {
+                    try
+                    {
+                        byte[] texture_data = Convert.FromBase64String(PNGData);
+                        if (texture_data != null)
+                        {
+                            texture = new Texture2D(size.x, size.y, textureFormat, mipCount, linear);
+                            texture.LoadImage(texture_data);
+                            texture.Apply();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.LogError(e);
+                    }
+                    if (texture == null)
+                    {
+                        texture = new Texture2D(size.x, size.y);
+                    }
+                }
+                return texture;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    if (value.isReadable)
+                    {
+                        sprite = null;
+                        texture = value;
+                        size = new Vector2Int(texture.width, texture.height);
+                        textureFormat = texture.format;
+                        pngData = Convert.ToBase64String(texture.EncodeToPNG());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sprite
+        /// </summary>
+        public Sprite Sprite
+        {
+            get
+            {
+                if (sprite == null)
+                {
+                    sprite = Sprite.Create(Texture, new Rect(0.0f, 0.0f, size.x, size.y), new Vector2(size.x * 0.5f, size.y * 0.5f));
+                }
+                return sprite;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    Texture = value.texture;
+                }
+            }
+        }
+    }
+}
